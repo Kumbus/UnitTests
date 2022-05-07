08d8d40bbb6a9a0b9e9788554903bda52bd613c7
Kumbus <72298393+Kumbus@users.noreply.github.com>
2022-05-07 21:49:48 +0200

    FizzBuzz

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -12,4 +12,21 @@
             return a * b;
         }
     }
+
+    public class FizzBuzz
+    {
+        public string GetOutput(int number)
+        {
+            if ((number % 3 == 0) && (number % 5 == 0))
+                return "FizzBuzz";
+
+            if (number % 3 == 0)
+                return "Fizz";
+
+            if (number % 5 == 0)
+                return "Buzz";
+
+            return number.ToString();
+        }
+    }
 }
